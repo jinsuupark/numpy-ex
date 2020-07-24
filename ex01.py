1ef9add1c4b1ed5a77c007f5094c4901d4b7ec00
jinsuupark <47841714+jinsuupark@users.noreply.github.com>
2020-07-24 18:04:06 +0900

    Initial commit

---
+++ ex01.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+data1 = [0, 1, 2 ,3, 4, 5]
+a1 =np.array(data1)
+print(a1)
+np.ar
+data2 = ([0.1, 1, 2, 3, 4, 0.5])
+a2 = np.array(data2)
+
+print(a2)
+
+print(a1.shape)
+
+print(a2.dtype)
